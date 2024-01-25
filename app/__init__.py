0e3e2f44cdb540a33bde71f49a976b339f06bb82
Andres Cabrera <andrescabrera@Accounts-iMac.local>
2024-01-25 16:47:33 -0500

    added stage dockerfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
-from firebase_admin import initialize_app, firestore
+import os
+import base64
+import json
+
+from firebase_admin import initialize_app, firestore, credentials
 from flask import Flask
 from flask_cors import CORS
 
 from config import Config
 
-
-firebase = initialize_app()
+encoded_credentials = os.environ.get("GOOGLE_CREDENTIALS")
+decoded_credentials = base64.b64decode(encoded_credentials)
+firebase_credential = credentials.Certificate(json.loads(decoded_credentials))
+firebase = initialize_app(firebase_credential)
 db = firestore.client()
 
 
