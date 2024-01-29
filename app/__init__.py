a5c7918c4decb461eaf242c53c6976784beb3d35
Andres Cabrera <andrescabrera@Accounts-iMac.local>
2024-01-29 09:20:28 -0500

    added logging to find issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 import base64
 import json
+import logging
 
 from firebase_admin import initialize_app, firestore, credentials
 from flask import Flask
@@ -9,10 +10,13 @@ from flask_cors import CORS
 from config import Config
 
 encoded_credentials = os.environ.get("GOOGLE_CREDENTIALS")
+logging.info("Found encoded credentials. Decoding...")
 if encoded_credentials:
     decoded_credentials = base64.b64decode(encoded_credentials)
+    logging.info("Decoded credentials.")
     firebase_credential = credentials.Certificate(json.loads(decoded_credentials))
     firebase = initialize_app(firebase_credential)
+    logging.info("Initialized firebase app.")
 else:
     firebase = initialize_app()
 db = firestore.client()
